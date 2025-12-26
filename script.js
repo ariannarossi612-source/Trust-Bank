1f71668e560a8a73b3249efd4ccbbcd6d8de951a
ariannarossi612-source <arianna.rossi612@gmail.com>
2025-12-27 00:44:06 +0100

    Update script.js
    “Add script.js”

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+  apiKey: "xxxx",
+  authDomain: "trust-bank-11868.firebaseapp.com",
+  projectId: "trust-bank-11868",
+  storageBucket: "trust-bank-11868.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "xxxx"
+};
+firebase.initializeApp(firebaseConfig);
+
 document.getElementById('login-btn').addEventListener('click', function() {
   let username = document.getElementById('username').value;
   let password = document.getElementById('password').value;
