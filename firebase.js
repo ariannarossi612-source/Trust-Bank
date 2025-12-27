34aac84cc5cd5cf23e22935c2bd7cf96bbf372b1
ariannarossi612-source <arianna.rossi612@gmail.com>
2025-12-27 11:56:59 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwjKTzj_1azYihm7jYxFYuIoM5OOFwZ58",
+  authDomain: "trust-bank-11868.firebaseapp.com",
+  projectId: "trust-bank-11868",
+  storageBucket: "trust-bank-11868.appspot.com",
+  messagingSenderId: "351989319032",
+  appId: "1:351989319032:web:7c7c413a80895cb8ef748d"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
